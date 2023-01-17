448ff1e6b5d639dd98967b4465434c1aa581d57d
Luigi Messina <luigi.messina@unimib.it>
2023-01-17 14:45:12 +0100

    - Modificata aggiunta pc per recuperare IP da hostname e non usare REMOTE_ADDR - Aggiunta vista wakeuplist/<ip>/ per elencare tutti i pc abilitati a WOL per una certa subnet - Aggiunto logging abilitazione/disabilitazione wakeonlan per un pc - Modificata vista enable/disable wol per ritornare errore se righe modificate == 0 - Aggiunto logging cancellazione pc

--- apollo.py
+++ apollo.py
@@ -10,6 +10,7 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
      
 import logging
 from logging.handlers import RotatingFileHandler
+import socket
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -18,7 +19,8 @@ app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'apollo.db'),
     SECRET_KEY='development key',
     USERNAME='admin',
-    PASSWORD='default'
+    PASSWORD='default',
+    DOMAIN='biblio.unimib.it'
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
@@ -42,6 +44,11 @@ def get_db():
     if not hasattr(g, 'sqlite_db'):
         g.sqlite_db = connect_db()
     return g.sqlite_db
+
+
+def resolve_ip(hostname):
+    print(app.config['DOMAIN'])
+    return socket.gethostbyname(hostname + '.' + app.config['DOMAIN'])
 
 @app.teardown_appcontext
 def close_db(error):
@@ -71,7 +78,7 @@ def nuovopc():
                     is_wol_node = 0
                     pass
                 db.execute('insert into computers (name,mac,ip,enabled,iswol) values (?, ?, ?, ?, ?)',
-                    [request.form["name"], request.form["mac"], request.environ['REMOTE_ADDR'], 1, is_wol_node])
+                    [str.upper(request.form["name"]), request.form["mac"], resolve_ip(request.form["name"]), 1, is_wol_node])
                 db.commit()
             except Exception:
                 error = "Errore nell'inserimento del nuovo pc. Computer esistente?"
@@ -97,38 +104,51 @@ def nuovopc():
 # Aggiunge un computer nel database
 # il flag is_wol_node determina se il computer
 # e' quello che svegliera' tutti gli altri nella stessa subnet
-@app.route('/computers/add', methods=['POST'])
-def add_computer():
+@app.route('/computers/add/<string:name>/<string:mac>/', methods=['POST'])
+def add_computer(name,mac):
     if request.method == "POST":
-        # verifica che il MAC address sia valido
-        if not bool(re.match('^' + '[\:\-]'.join(['([0-9a-f]{2})']*6) + '$', request.json["mac"].lower())):
-            return make_response("",422)
+        try:
+            # verifica che il MAC address sia valido
+            if not bool(re.match('^' + '[\:\-]'.join(['([0-9a-f]{2})']*6) + '$', mac.lower())):
+                return make_response("%s %s" %(name,mac),422)
+        except:
+            return make_response("INvalid parameters",400)
         else:
             db = get_db()
             try:
                 db.execute('insert into computers (name,mac,ip,enabled,iswol) values (?, ?, ?, ?, ?)',
-                    [str.upper(request.json["name"]), request.json["mac"], request.environ['REMOTE_ADDR'], 1, request.json["is_wol_node"]])
+                    [str.upper(name), mac.lower(), request.environ['REMOTE_ADDR'], 1, 0])
                 db.commit()
             except:
                 return make_response("",409)
             else:
-                return make_response("", 200)
+                return make_response("Inserito %s %s" % (name,mac), 201)
 
 @app.route('/computers/<name>/enable/',methods=['PUT','POST'])
 def enable(name):
     if request.method == "POST" or request.method == "PUT":
         db = get_db()
-        db.execute('update computers set enabled = 1 where name = ?',[name])
+        cur = db.execute('update computers set enabled = 1 where name = ?',[str.upper(name)])
         db.commit()
-        return make_response("", 201)
+        if cur.rowcount > 0:
+            app.logger.info('Enabled Wake On Lan for %s' % name)
+            return make_response("", 201)
+        else:
+            app.logger.warning('Failed attempt to enable wake on lan for non existing host:%s' % name)
+            return make_response("ERROR: Not found",404)
 
 @app.route('/computers/<name>/disable/',methods=['PUT','POST'])
 def disable(name):
     if request.method == "POST" or request.method == "PUT":
         db = get_db()
-        db.execute('update computers set enabled = 0 where name = ?',[name])
+        cur = db.execute('update computers set enabled = 0 where name = ?',[str.upper(name)])
         db.commit()
-        return make_response("", 201)
+        if cur.rowcount > 0:
+            app.logger.info('Disabled Wake On Lan for %s' % name)
+            return make_response("", 201)
+        else:
+            app.logger.warning('Failed attempt to disable wake on lan for non existing host:%s' % name)
+            return make_response("ERROR: Not found",404)
 
 @app.route('/computers/<name>/remove/',methods=['DEL','POST'])
 def remove(name):
@@ -137,7 +157,7 @@ def remove(name):
         try:
             cur = db.execute('delete from computers where name = ?',[name])
             deleted = cur.rowcount
-            #app.logger.debug(deleted)
+            app.logger.info('Removed %i rows for computer %s' % (cur.rowcount,name))
             db.commit()
         except:
             return make_response("", 400)
@@ -167,14 +187,32 @@ def wol_node_deactivate(name):
 # Ritorna un elenco hostname,macaddress
 # di tutti i computer da svegliare che fanno parte
 # della stessa subnet dell'ip da cui proviene la richiesta
+@app.route('/indecent/wakeuplist/', methods=['GET'])
+def computers_to_be_rudely_awakened():
+    if request.environ['REMOTE_ADDR']:
+        db = get_db()
+        cur = db.execute('select name,mac from computers where enabled = 1 and iswol = 0 and ip like ?',['%s%%' % '.'.join(request.environ['REMOTE_ADDR'].split('.')[0:3])])
+        mac_list = map(list,cur.fetchall())
+        lista = ""
+        for computer in mac_list:
+            lista+="C:\Temp\WakeOnLan\mac-wol.exe %s /a 149.132.118.255\n" % computer[1]
+        return lista
+
 @app.route('/wakeuplist/', methods=['GET'])
 def computers_to_be_awakened():
     if request.environ['REMOTE_ADDR']:
         db = get_db()
         cur = db.execute('select name,mac from computers where enabled = 1 and iswol = 0 and ip like ?',['%s%%' % '.'.join(request.environ['REMOTE_ADDR'].split('.')[0:3])])
         mac_list = map(list,cur.fetchall())
-        return jsonify(mac_list),200
+        return make_response(jsonify(list(mac_list)),200)
 
+@app.route('/wakeuplist/<ip>/', methods=['GET'])
+def computers_to_be_awakened_by_ip(ip):
+        db = get_db()
+        cur = db.execute('select name,mac from computers where enabled = 1 and iswol = 0 and ip like ?',['%s%%' %
+        ('.'.join(ip.split('.')[0:3]) +'.')])
+        mac_list = map(list,cur.fetchall())
+        return make_response(jsonify(list(mac_list)),200)
 
 if __name__ == '__main__':
     handler = RotatingFileHandler('apollo.log', maxBytes=10000, backupCount=1)
